31890d62efc92550c0f0a853ec7b109b64d3609b
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-22 11:21:57 +0430

    options are deprecated

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -2,9 +2,9 @@ import { DB_HOST } from '@config';
 const db_url = DB_HOST || 'mongodb://localhost:27017/tribePayment';
 export const dbConnection = db_url && {
   url: db_url,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  },
+  // options: {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false,
+  // },
 };
