5937c17d02a0304b38061fee5fbe7d9068a61126
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-24 01:34:13 +0430

    some new feature for user

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,13 +1,24 @@
 import { model, Schema, Document } from 'mongoose';
-import { User } from '@interfaces/users.interface';
-
-const userSchema: Schema = new Schema({
+//import { User } from '@interfaces/users.interface';
+interface userInterface extends Document {
+  name?: string;
+  email: string;
+  wallet: number;
+}
+const userSchema: Schema = new Schema<userInterface>({
   name: {
+    type: String,
+  },
+  email: {
     type: String,
     required: true,
   },
+  wallet: {
+    type: Number,
+    default: 0,
+  },
 });
 
-const userModel = model<User & Document>('User', userSchema);
+const userModel = model<userInterface>('User', userSchema);
 
 export default userModel;
