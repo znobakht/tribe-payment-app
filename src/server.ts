08b17e85cb23d8384e1ead5f43aa632c32bdb939
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-25 20:24:26 +0430

    userRoute added

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import HealthRoute from '@routes/health.route';
 import IndexRoute from '@routes/index.route';
 import PaymentRoute from './routes/payment.route';
 import WebhookRoute from '@/routes/webhook.route';
+import UserRoute from './routes/user.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
 const app = new App({
-  client: [new IndexRoute(), new HealthRoute(), new PaymentRoute()],
+  client: [new IndexRoute(), new HealthRoute(), new PaymentRoute(), new UserRoute()],
   server: [new WebhookRoute()],
 });
 
