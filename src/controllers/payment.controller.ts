dec5b9d8165d4d5f5fc054132e01709549581364
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-25 15:42:28 +0430

    check for necessary fields

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -5,6 +5,9 @@ import { NextFunction, Request, Response } from 'express';
 class PaymentController {
   public pay = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      if (!req.body.email || !req.body.amount) {
+        return res.status(400).json({ message: 'Missing required fields' });
+      }
       const { email, amount } = req.body;
       let userObject = await userModel.findOne({ email });
       if (userObject) {
