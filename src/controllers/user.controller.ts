52361a801ecf2fc4ec10f028941078b5f2721934
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-25 20:24:53 +0430

    register user method

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,28 @@
+import userModel from '@/models/users.model';
 import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
 
 class userController {
   public registerUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.render('home', { title: 'Starter App' });
+      if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'Missing required fields' });
+      }
+      const { email, password } = req.body;
+      const userObject = await userModel.findOne({ email });
+      if (userObject) {
+        return res.status(400).json({ message: 'This email is in use' });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const newPass = await bcrypt.hash(password, salt);
+      const profile = await userModel.create({
+        email,
+        password: newPass,
+      });
+      const userForSend: any = _.pick(profile, ['_id', 'name', 'email']);
+
+      return res.status(200).json({ message: 'account created successfully', profile: userForSend });
     } catch (error) {
       next(error);
     }
