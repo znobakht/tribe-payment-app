e61262ea10b81b7426b6bcc7f0ec86527031587c
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-28 02:22:50 +0430

    logout and getToken controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import userModel from '@/models/users.model';
 import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
+import refreshTokenModel from '@/models/refreshToken.model';
+import { v4 as uuidv4 } from 'uuid';
 
 class userController {
   public registerUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -43,8 +45,52 @@ class userController {
         return res.status(404).json({ message: 'password is incorrect' });
       }
       const token = userObject.generateAuthToken();
+      const refreshToken = uuidv4();
+      await refreshTokenModel.create({ user: userObject._id, token, refreshToken });
       const userForSend: any = _.pick(userObject, ['_id', 'name', 'email']);
-      res.status(200).json({ token: 'Bearer ' + token, profile: userForSend });
+      res.status(200).json({ token, profile: userForSend, refreshToken });
+    } catch (error) {
+      next(error);
+    }
+  };
+  public logoutUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (!req.headers.refreshToken) {
+        return res.status(400).json({ message: 'Missing required fields' });
+      }
+
+      const rtObject = await refreshTokenModel.findOne({ refreshToken: req.headers.refreshToken });
+
+      if (!rtObject) {
+        return res.status(400).json({ message: 'you are not logged in' });
+      }
+      await refreshTokenModel.deleteMany({ refreshToken: req.headers.refreshToken });
+      return res.status(200).json({ message: 'you logged out successfully' });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+  public getToken = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (!req.headers.refreshToken) {
+        return res.status(400).json({ message: 'Missing required fields' });
+      }
+
+      const refreshToken = req.headers.refreshToken;
+      const rtObject = await refreshTokenModel.findOne({ refreshToken });
+
+      if (!rtObject) {
+        return res.status(400).json({ message: 'you are not logged in' });
+      }
+      const userObject = await userModel.findById(rtObject.user);
+      if (!userObject) {
+        return res.status(400).json({ message: 'user doesnt exist' });
+      }
+      const token = userObject.generateAuthToken();
+
+      res.status(200).json({ token, refreshToken });
+      next();
     } catch (error) {
       next(error);
     }
