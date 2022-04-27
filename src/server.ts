e9d7b848c33439d321ccdde9f894c4e0744041dd
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-28 02:41:41 +0430

    1234

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,15 @@ import WebhookRoute from '@/routes/webhook.route';
 import UserRoute from './routes/user.route';
 import validateEnv from '@utils/validateEnv';
 
+console.log('test0');
 validateEnv();
+console.log('test1');
 
 const app = new App({
   client: [new IndexRoute(), new HealthRoute(), new PaymentRoute(), new UserRoute()],
   server: [new WebhookRoute()],
 });
+console.log('test2');
 
 app.listen();
+console.log('test3');
