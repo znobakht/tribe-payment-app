c1d609def292492401c7cd921fa6ec684322260b
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-29 01:54:41 +0430

    a model for RTs

---
+++ src/models/refreshToken.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema, Document } from 'mongoose';
+
+export interface RefreshTokenInterface extends Document {
+  token: string;
+  refreshToken?: string;
+  user?: string;
+  expire: Date;
+}
+const RefreshTokenSchema: Schema = new Schema<RefreshTokenInterface>({
+  token: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  expire: {
+    type: Date,
+    default: Date.now,
+    expires: 60 * 60 * 24 * 7,
+  },
+});
+
+export default model<RefreshTokenInterface>('RefreshToken', RefreshTokenSchema);
