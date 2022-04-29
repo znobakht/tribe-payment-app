b20963192cc5449f29b043ad3b8ac78b1269cd9f
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-29 04:32:45 +0430

    check if member added to community or not

--- src/controllers/join.controller.ts
+++ src/controllers/join.controller.ts
@@ -3,10 +3,12 @@ import userModel from '@/models/users.model';
 import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { joinPrice } from '@/config';
+import MemberController from './member.controller';
 class PaymentController {
+  public memberCtrl = new MemberController();
   public joinWithPass = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      if (!req.body.email || !req.body.password) {
+      if (!req.body.email || !req.body.password || !req.body.name) {
         return res.status(400).json({ message: 'Missing required fields' });
       }
       const { password } = req.body;
@@ -15,14 +17,22 @@ class PaymentController {
       if (!userObject) {
         return res.status(400).json({ message: 'user doesnt exist' });
       }
-      const isMatch = bcrypt.compare(password, userObject.password);
+      const isMatch = await bcrypt.compare(password, userObject.password);
       if (!isMatch) {
         return res.status(404).json({ message: 'password is incorrect' });
       }
-      userObject.wallet -= joinPrice;
-      userObject.save();
-      await transactionModel.create({ email, joinPrice, type: 'buy' });
-      res.json({ message: 'email added to community' });
+      if (userObject.wallet < joinPrice) {
+        return res.status(404).json({ message: 'you need to charge your account' });
+      }
+      const newMember: any = await this.memberCtrl.addAMember(req);
+      if (newMember?.accessToken) {
+        userObject.wallet -= joinPrice;
+        userObject.save();
+        await transactionModel.create({ email, amount: joinPrice, type: 'buy' });
+        res.json({ message: 'email added to community', accessToken: newMember.accessToken, refreshToken: newMember.refreshToken });
+      } else {
+        res.status(400).send(newMember);
+      }
     } catch (error) {
       next(error);
     }
